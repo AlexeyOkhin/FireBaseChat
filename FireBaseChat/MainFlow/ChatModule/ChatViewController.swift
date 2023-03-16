6306db08cab69e7808cbd57c3d49f5a16f78f6f9
AlexeyOkhin <k-i-ll-e-r@list.ru>
2023-03-16 17:29:40 +0500

    add chat screen

---
+++ FireBaseChat/MainFlow/ChatModule/ChatViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ChatViewController.swift
+//  FireBaseChat
+//
+//  Created by Djinsolobzik on 16.03.2023.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+
+
+}
