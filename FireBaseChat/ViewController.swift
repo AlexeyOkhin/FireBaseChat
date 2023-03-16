313bc5f59ae7216c26f67205619262794ab22d46
AlexeyOkhin <k-i-ll-e-r@list.ru>
2023-03-16 12:57:17 +0500

    Initial Commit

---
+++ FireBaseChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FireBaseChat
+//
+//  Created by Djinsolobzik on 16.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
